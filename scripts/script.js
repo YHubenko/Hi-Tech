63624b62bdeca8182cb5f080d9dc279cf33c2945
Yehor Hubenko <112268293+YHubenko@users.noreply.github.com>
2022-11-13 18:28:18 +0200

    Add burger menu

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,31 @@
 let scrollPosition = window.pageYOffset;
 localStorage.setItem('cords', scrollPosition);
-let header = document.querySelector('header');
+let headers = document.querySelectorAll('header');
 let logo = document.querySelector('.logo');
+let burger = document.querySelector('.burger');
+let burgerSource = "images/burger.png";
+let scrollFlag = false;
 
 window.addEventListener('scroll', () => {
     let newScrollPosition = window.pageYOffset;
     if (scrollPosition === newScrollPosition) {
-        header.classList.remove('scrolled');
-        logo.src = "images/logo.png";
+        for (const header of headers) {
+            if (!burgerFlag) {
+                header.classList.remove('scrolled');
+                logo.src = "images/logo.png";
+                burgerSource = burger.src = "images/burger.png";
+            }
+        }
+        scrollFlag = false;
     } else {
-        header.classList.add('scrolled');
-        logo.src = "images/logo-dark.png";
+        for (const header of headers) {
+            if (!burgerFlag) {
+                header.classList.add('scrolled');
+                logo.src = "images/logo-dark.png";
+                burgerSource = burger.src = "images/burger-dark.png";
+            }
+        }
+        scrollFlag = true;
     }
 })
 
@@ -30,3 +45,24 @@ for (const aboutObject of aboutObjects) {
         }
     })
 }
+
+let burgerFlag = false;
+burger.addEventListener('click', () => {
+    if (!burgerFlag) {
+        burgerFlag = true;
+        burger.src = "images/burger-active.png";
+        burger.style.transform = 'rotate(180deg)';
+        for (const header of headers) {
+            header.style.transform = 'none';
+            header.classList.add('scrolled');
+        }
+    } else {
+        burgerFlag = false;
+        burger.src = burgerSource;
+        burger.style.transform = 'rotate(360deg)';
+        for (const header of headers) {
+            header.style.transform = 'translate(0, -210px)';
+            if (!scrollFlag) header.classList.remove('scrolled');
+        }
+    }
+})
